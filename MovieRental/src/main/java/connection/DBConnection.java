dae0e3745774eb0e6ceca80cdf4430499da12c86
fundacion <pfundacionsegundosdevida@gmail.com>
2022-09-02 21:46:21 -0500

    Creando la coneccion con la BD

--- MovieRental/src/main/java/connection/DBConnection.java
+++ MovieRental/src/main/java/connection/DBConnection.java
@@ -8,6 +8,13 @@ package connection;
  *
  * @author Usuario
  */
+import java.sql.Connection; //importando la dependica para la coneccion
+import java.sql.DriverManager; //controlador de bd
 public class DBConnection {
-    
+    //proseso de conexion entre el bac y la BD
+    Connection connection; //creando objeto conexion
+    static String bd = "movies_rental"; //colocamos el nombre de la bd
+    static String port = "3306"; //puerto de la bd, toca revisar en el gertor de bd
+    static String login = "root";
+    static String password = "admin"; //contraseña de la base de datos    
 }
