85e63fed7876f34b4be75b4b3a5ea2321349afe5
fundacion <pfundacionsegundosdevida@gmail.com>
2022-09-02 21:17:26 -0500

    Agregando variales, contructor get y set y tsting() de la clase alquiler

--- MovieRental/src/main/java/beans/Alquiler.java
+++ MovieRental/src/main/java/beans/Alquiler.java
@@ -4,10 +4,78 @@
  */
 package beans;
 
+import java.sql.Date;//importar fehca y hora como sql
+
 /**
  *
  * @author Usuario
  */
 public class Alquiler {
+    //atributos de la tabla alquiler en la BD 
+    private int id;
+    private String username;
+    private Date fecha;
+    //datos dicionales no estan en la tabla pero la necesitamos para mostrar esos datos
+    private boolean novedad;
+    private String genero;
+    
+// contructor 
+    public Alquiler(int id, String username, Date fecha, boolean novedad, String genero) {
+        this.id = id;
+        this.username = username;
+        this.fecha = fecha;
+        this.novedad = novedad;
+        this.genero = genero;
+    }
+    //getters y setter 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public boolean isNovedad() {
+        return novedad;
+    }
+
+    public void setNovedad(boolean novedad) {
+        this.novedad = novedad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    // tostring()
+
+    @Override
+    public String toString() {
+        return "Alquiler{" + "id=" + id + ", username=" + username + ", fecha=" + fecha + ", novedad=" + novedad + ", genero=" + genero + '}';
+    }
+    
+    
+    
     
 }
