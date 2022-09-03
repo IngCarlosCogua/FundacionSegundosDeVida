6e7193f8e99b9e96323c1920e3807b52c42ecb39
fundacion <pfundacionsegundosdevida@gmail.com>
2022-09-02 21:07:49 -0500

    creando variablesconstructor, geter an seter y tostring de la clase Usuario

--- MovieRental/src/main/java/beans/Usuario.java
+++ MovieRental/src/main/java/beans/Usuario.java
@@ -9,5 +9,91 @@ package beans;
  * @author Usuario
  */
 public class Usuario {
+    //Atributos de la tabla en la BD USUARIO
+    private String username;
+    private String contrasena;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private double saldo;
+    private boolean premiun;
+    
+//constructor
+    public Usuario(String username, String contrasena, String nombre, String apellido, String email, double saldo, boolean premiun) {
+        this.username = username;
+        this.contrasena = contrasena;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.saldo = saldo;
+        this.premiun = premiun;
+    }
+    
+// getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isPremiun() {
+        return premiun;
+    }
+
+    public void setPremiun(boolean premiun) {
+        this.premiun = premiun;
+    }
+    
+    // ----tostring-------
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "username=" + username + ", contrasena=" + contrasena + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", saldo=" + saldo + ", premiun=" + premiun + '}';
+    }
+    
+  
+
     
 }
